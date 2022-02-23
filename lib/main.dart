0ead2d11d2b29155ec83c66148900223d7854e46
djmel1955@gmail.com <djmel1955@gmail.com>
2022-02-23 21:14:58 +0000

    flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,32 @@
 
 import 'package:flutter/material.dart';
 
+
+var list = ["mustapha", "samir", "fares", "abdelkader", "yacine"];
+var i = 0;
+  
+class RandomWords extends StatefulWidget {
+  const RandomWords({ Key? key }) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _RandomWordsState extends State<RandomWords> {
+    @override
+    Widget build(BuildContext context) {
+      final wordPair = list[i];
+      return Text(wordPair);
+    }
+  }
+
 void main() {
   runApp(
     MaterialApp(
@@ -13,11 +39,14 @@ void main() {
           centerTitle: true,
           backgroundColor: const Color.fromARGB(255, 182, 31, 46),
         ),
-        body: const Center(
-          child: Text('mustapha your are bigger'),
+        body: const Center(child:  Text(RandomWords()),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            },
+          child: const Icon(Icons.navigation),
         ),
-        backgroundColor: const Color.fromARGB(255, 78, 224, 83),
       ),
     ),
   );
 }
+
