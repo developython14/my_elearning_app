deaab5aeb5b81f3c09027782b3b7942064186f19
djmel1955@gmail.com <djmel1955@gmail.com>
2022-02-28 21:53:30 +0000

    first uploa

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,43 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
 void main() {
-  var list = ["mustapha", "samir", "fares", "abdelkader", "yacine"];
-  var i = 0;
-  var name = list[0];
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('rigth app'),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 233, 106, 119),
-        ),
-        body: Row(
-          children: <Widget>[
-            Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  color: Color.fromARGB(255, 97, 233, 102),
-                )),
-            Expanded(
-                flex: 2,
-                child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  color: Colors.amber,
-                ))
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {i = (i + 1) % 4, name = list[i]},
-          child: Icon(Icons.account_box),
-        ),
-      ),
-    ),
+    MaterialApp(home: mustapha()),
   );
+}
+
+class mustapha extends StatefulWidget {
+  const mustapha({Key? key}) : super(key: key);
+
+  @override
+  _mustaphaState createState() => _mustaphaState();
+}
+
+class _mustaphaState extends State<mustapha> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('mustapha application'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 111, 255, 116),
+      ),
+      body: Column(
+        children: [
+          Text("hi i'm mustrapha  belkassem"),
+          Text("yes i'm guerrier")
+        ],
+        mainAxisAlignment: Center,
+        crossAxisAlignment: Center,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.power_off_outlined),
+        focusColor: Colors.amber,
+        foregroundColor: Colors.black,
+      ),
+    );
+  }
 }
