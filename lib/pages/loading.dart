b1b0095982735fb23fe8ef8e891c6455e24e4f73
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-02 18:18:10 +0000

    structure package

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,16 +12,16 @@ class _loadingState extends State<loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Color.fromARGB(255, 8, 230, 15),
+      backgroundColor: Color.fromARGB(255, 8, 230, 15),
       appBar: AppBar(
         title: Text('mustapha'),
         centerTitle: true,
         foregroundColor: Color.fromARGB(255, 2, 141, 255),
       ),
       body: Center(
-        child: SpinKitSpinningLines(
+        child: SpinKitCubeGrid(
           color: Colors.white,
-          size: 80.0,
+          size: 100.0,
         ),
       ),
       floatingActionButton: FloatingActionButton(
